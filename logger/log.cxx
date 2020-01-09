f8a73903ed2a9a375e1721ef71833cf00d2f3e02
ShixianGuo <729841861@qq.com>
2020-01-09 23:51:09 +0800

    logger

--- logger/log.cxx
+++ logger/log.cxx
@@ -7,29 +7,38 @@ namespace GS{
       
   }
 
-  void Logger::log(LogLevel::Level level,LogEvent::ptr event);
+  void Logger::log(LogLevel::Level level,LogEvent::ptr event){
+     
+  }
 
   void Logger::debug(LogEvent::ptr event){
-
+        log(LogLevel::DEBUG, event);
   }
   void Logger::info(LogEvent::ptr event){
-      
+       log(LogLevel::DEBUG, event);
   }
   void Logger::warn(LogEvent::ptr event){
-      
+       log(LogLevel::DEBUG, event);
   }
   void Logger::error(LogEvent::ptr event){
-      
+       log(LogLevel::DEBUG, event);
   }
   void Logger::fatal(LogEvent::ptr event){
-      
+       log(LogLevel::DEBUG, event);
   }
 
   void Logger::addAppender(LogAppender::ptr appender){
       m_appenders.push_back(appender);
+      
+      
   }
   void Logger::delAppender(LogAppender::ptr appender){
-      
+      for(auto it=m_appenders.begin();it!=m_appenders.end();++it){
+          if(*it==appender){
+             m_appenders.erase(it);
+             breadk;
+          }
+      }
   }
 
 }
